1920e9d0ec5c6a5c6b19521490655630024fc1cd
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2023-12-03 21:00:37 -0500

    Implementation of the modular multiplicative inverse

--- arith.zig
+++ arith.zig
@@ -1,6 +1,39 @@
 const std = @import("std");
 const mul = std.math.mul;
 const Int = std.meta.Int;
+const Tuple = std.meta.Tuple;
+
+/// Modular multiplicative inverse
+///
+/// Based on the extended Euclidean algorithm
+pub fn invmod(number: isize, modulus: usize) !usize {
+    if (modulus == 0) return error.DivisionByZero;
+    if (modulus == 1) return error.NotInvertible;
+
+    // TODO Replace `Tuple(&.{ isize, usize })` with `struct { isize, usize }`
+
+    // TODO Replace with `@abs(number)`
+    var prev: Tuple(&.{ isize, usize }) = .{ 1, @intCast(usize, if (number < 0) -number else number) };
+    var curr: Tuple(&.{ isize, usize }) = .{ 0, modulus };
+
+    while (curr[1] != 0) {
+        const quotient = @intCast(isize, prev[1] / curr[1]);
+        const remainder = prev[1] % curr[1];
+
+        // `remainder` is equivalent to `prev[1] - quotient * curr[1]`
+        const next: Tuple(&.{ isize, usize }) = .{ prev[0] - quotient * curr[0], remainder };
+        prev = curr;
+        curr = next;
+    }
+
+    if (prev[1] != 1) {
+        return error.NotInvertible;
+    }
+
+    // TODO Replace with `@abs(prev[0])`
+    const result = @intCast(usize, if (prev[0] < 0) -prev[0] else prev[0]);
+    return if ((number < 0) != (prev[0] < 0)) modulus - result else result;
+}
 
 /// Modular multiplication
 pub fn mulmod(comptime T: type, a: T, b: T, m: T) !T {
