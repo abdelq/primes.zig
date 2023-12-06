2387d2301bc47e830a26d7a349958db4fe01d004
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2023-12-06 18:05:28 -0500

    Test for base 2 pseudoprimes and minor perf tweaks

--- test.zig
+++ test.zig
@@ -44,18 +44,16 @@ pub fn miller_rabin(number: usize, base: usize) bool {
         return number == 2;
     }
 
-    // Factoring powers of 2 from `number - 1`
-    var d = number - 1;
-    while (d % 2 == 0) {
-        d /= 2;
-    }
+    // Expressing `number - 1` as `2ˢd`
+    var s = @ctz(number - 1);
+    const d = math.shr(usize, number - 1, s);
 
     var x = powmod(usize, base, d, number) catch unreachable;
     if (x == 1 or x == number - 1) {
         return true;
     }
 
-    return while (d != number - 1) : (d *= 2) {
+    return while (s > 1) : (s -= 1) {
         x = powmod(usize, x, 2, number) catch unreachable;
         // zig fmt: off
         if (x == 1)          break false;
@@ -79,6 +77,7 @@ fn jacobi_symbol(upper: isize, lower: usize) !isize {
     var a = @intCast(usize, if (upper < 0) -upper else upper);
     var n = lower;
     a %= n;
+
     while (a != 0) : (a %= n) {
         while (a % 2 == 0) : (a /= 2) {
             if (n % 8 == 3 or n % 8 == 5) {
@@ -92,4 +91,24 @@ fn jacobi_symbol(upper: isize, lower: usize) !isize {
     }
 
     return if (n == 1) result else 0;
+}
+
+test "Strong base 2 pseudoprimes" {
+    // Strong pseudoprimes to base 2 (oeis.org/A001262)
+    const pseudoprimes = [_]usize{
+        2047,   3277,   4033,   4681,   8321,   15841,  29341,  42799,  49141,
+        52633,  65281,  74665,  80581,  85489,  88357,  90751,  104653, 130561,
+        196093, 220729, 233017, 252601, 253241, 256999, 271951, 280601, 314821,
+        357761, 390937, 458989, 476971, 486737,
+    };
+
+    var num: usize = 0;
+    while (num <= pseudoprimes[pseudoprimes.len - 1]) : (num += 1) {
+        // zig fmt: off
+        try std.testing.expect(
+            miller_rabin(num, 2) == trial_division(num) or
+            mem.indexOfScalar(usize, &pseudoprimes, num) != null
+        );
+        // zig fmt: on
+    }
 }
